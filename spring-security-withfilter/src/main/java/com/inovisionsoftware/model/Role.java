4e0e2e616db059834d2f7ec5bc2a97100b7d5d01
Kunal Patil <kunalpatil@yahoo.com>
2018-02-21 16:37:33 -0500

    Initial Commit

---
+++ spring-security-withfilter/src/main/java/com/inovisionsoftware/model/Role.java
@@ -0,0 +1,23 @@
+package com.inovisionsoftware.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Role implements GrantedAuthority {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String getAuthority() {
+		return name;
+	}
+	
+	
+}
